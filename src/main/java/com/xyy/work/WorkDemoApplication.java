eebdf5d9ab11ce0196412745eb153706c140bca8
幻听嵩的留香 <1823809284@qq.com>
2026-01-01 21:42:46 +0800

    创建springboot项目

---
+++ src/main/java/com/xyy/work/WorkDemoApplication.java
@@ -0,0 +1,13 @@
+package com.xyy.work;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkDemoApplication.class, args);
+    }
+
+}
