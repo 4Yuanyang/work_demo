c25724be6ab06680b7552f70ee3263d62db95d3e
xyy <xyy@dnjfashion.com>
2026-01-02 15:28:31 +0800

    之前用户没设置name和email，导致提交上来显示为unknow，已修改

--- src/main/java/com/xyy/work/mapstruct/UserDTO.java
+++ src/main/java/com/xyy/work/mapstruct/UserDTO.java
@@ -1,12 +1,11 @@
 package com.xyy.work.mapstruct;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
-@JsonIgnoreProperties(ignoreUnknown = true)
+//@JsonIgnoreProperties(ignoreUnknown = true) //这个注解是在需要json转换时候用的，目前用不到，可以去掉
 public class UserDTO {
     private String name;
     private Integer age;
