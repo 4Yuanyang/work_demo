755e074fddc4499f3a2842fe4550fa8922ee943f
xyy <xyy@dnjfashion.com>
2026-02-05 17:44:44 +0800

    测试idea上git操作

--- src/test/java/com/xyy/work/WorkDemoApplicationTests.java
+++ src/test/java/com/xyy/work/WorkDemoApplicationTests.java
@@ -5,6 +5,8 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
+import java.util.Optional;
+
 @SpringBootTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 // 或者完全禁用数据源配置
@@ -15,6 +17,18 @@ class WorkDemoApplicationTests {
 
     @Test
     void contextLoads() {
+
+    }
+
+    @Test
+    void testOptional() {
+        Optional<String> optional = Optional.of("hello");
+        System.out.println(optional.orElse("xyy"));
+        optional.ifPresent(System.out::println);
+
+        System.out.println(optional.map(String::toUpperCase)
+                .map(String::toCharArray)
+                .orElse(new char[]{1})[0]);
 
     }
 
